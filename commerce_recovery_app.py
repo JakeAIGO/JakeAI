afb1262a2253344fa0db9b1a42a77a0d7c494798
JakeAIGO <jason@solutionser.com>
2026-09-19 18:06:53 -0400

    Register encrypted JakeAI Secret Vault

--- commerce_recovery_app.py
+++ commerce_recovery_app.py
@@ -212,3 +212,6 @@ def genesis_review_decline_refund_runtime(
 # ---------------------------------------------------------------------------
 from direct_billing import register_direct_routes
 register_direct_routes(app)
+
+from secret_vault import register_secret_vault_routes
+register_secret_vault_routes(app)
